2fda0854a80b2e398ebaa5dea523234e64b92e2f
raphael meyer <raphael.akira.meyer@gmail.com>
2018-04-23 07:05:43 +0200

    start using networking TS

--- source/ccucumber/WireServer.cc
+++ source/ccucumber/WireServer.cc
@@ -9,20 +9,32 @@
 #include <iostream>
 #include <iterator>
 
+#include <experimental/net>
+#include <experimental/socket>
+
 using nlohmann::json;
 using namespace std::string_literals;
+
+namespace net = std::experimental::net;
 
 namespace ccucumber {
 
 void WireServer::run(std::uint16_t port) {
-  TcpServer server;
-  server.listen(port);
 
-  do {
-    auto const line = server.read_line();
+  net::io_context context;
+  net::ip::tcp::endpoint endpoint{net::ip::tcp::v4(), port};
+  net::ip::tcp::acceptor acceptor{context, endpoint};
+
+  acceptor.set_option(net::ip::tcp::acceptor::reuse_address{true});
+  acceptor.accept(context);
+
+  // todo get stream from socket
+  net::ip::tcp::iostream stream{acceptor};
+
+  std::string line;
+  while(stream && std::getline(stream, line)) {
     // handler.process(line);
-
-  } while(not server.connection_is_open());
+  }
 }
 
 } // namespace ccucumber
